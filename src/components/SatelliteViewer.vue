ca769befebed0a930432f4d36a379bbcc98a4d01
Saknet <andreas.v.andra@gmail.com>
2024-07-11 22:56:56 +0300

    tifs from url instead of public folder

--- src/components/SatelliteViewer.vue
+++ src/components/SatelliteViewer.vue
@@ -30,23 +30,23 @@ import TileLayer from 'ol/layer/WebGLTile.js';
 
 let map; // Variable to hold the map instance
 
-const selectedSatellite = ref('2023-08-15_Sentinel-2_L2A_NDVI.tiff'); // Default selected satellite image
+const selectedSatellite = ref('https://geo.fvh.fi/spotted/data/sentinel_images/2023-08-15_Sentinel-2_L2A_NDVI.tiff'); // Default selected satellite image
 const satelliteOptions = [
-  { text: 'June 14, 2018', value: '2018-06-14_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'June 21, 2020', value: '2020-06-21_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'June 26, 2022', value: '2022-06-26_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'January 27, 2023', value: '2023-01-27_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'February 26, 2023', value: '2023-02-26_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'March 15, 2023', value: '2023-03-15_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'April 22, 2023', value: '2023-04-22_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'May 24, 2023', value: '2023-05-24_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'June 23, 2023', value: '2023-06-23_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'July 13, 2023', value: '2023-07-13_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'August 15, 2023', value: '2023-08-15_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'September 14, 2023', value: '2023-09-14_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'October 29, 2023', value: '2023-10-29_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'November 25, 2023', value: '2023-11-25_Sentinel-2_L2A_NDVI.tiff' },
-  { text: 'December 28, 2023', value: '2023-12-28_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'June 14, 2018', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2018-06-14_Sentinel-2_L2A_NDVI.tiff' }, 
+  { text: 'June 21, 2020', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2020-06-21_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'June 26, 2022', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2022-06-26_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'January 27, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-01-27_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'February 26, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-02-26_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'March 15, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-03-15_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'April 22, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-04-22_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'May 24, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-05-24_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'June 23, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-06-23_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'July 13, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-07-13_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'August 15, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-08-15_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'September 14, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-09-14_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'October 29, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-10-29_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'November 25, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-11-25_Sentinel-2_L2A_NDVI.tiff' },
+  { text: 'December 28, 2023', value: 'https://geo.fvh.fi/spotted/data/sentinel_images/2023-12-28_Sentinel-2_L2A_NDVI.tiff' },
 ];
 
 const loadSatelliteImage = (value) => {
@@ -65,7 +65,7 @@ const initializeMap = (imagePath) => {
   const source = new GeoTIFF({
     sources: [
       {
-        url: `/assets/images/satellite/${imagePath}`,
+        url: imagePath,
         min: 0,
         max: 1,
       },
